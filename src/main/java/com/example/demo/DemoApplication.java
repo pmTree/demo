7fb80f93826e07679d7de85b08d17eea100b181d
pm <pengmeng@hdkjrd.com>
2019-01-11 11:58:38 +0800

    test
    1233

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,6 +3,7 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//jjj
 @SpringBootApplication
 public class DemoApplication {
 
